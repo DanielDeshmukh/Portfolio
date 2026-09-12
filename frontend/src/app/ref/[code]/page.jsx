791b0519c67b2b3e37568b355f0eaca23b8429ac
DanielDeshmukh <deshmukhdaniel2005@gmail.com>
2026-09-12 20:05:15 +0530

    fix: re-add missing Link import in ref page

--- frontend/src/app/ref/[code]/page.jsx
+++ frontend/src/app/ref/[code]/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
+import Link from 'next/link'
 
 export default function RefPage() {
   const { code } = useParams()
